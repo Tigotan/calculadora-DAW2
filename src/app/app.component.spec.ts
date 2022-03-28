e752e8c05dd5b1e9f770e7b5dc37086b46b44210
Tigotan Jiménez Quintana <tigotan.jimenez@gmail.com>
2022-03-28 11:36:43 +0100

    Updated the project with the src code from class

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'calculadora-DAW2'`, () => {
+  it(`should have as title 'angularTexto'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('calculadora-DAW2');
+    expect(app.title).toEqual('angularTexto');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('calculadora-DAW2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angularTexto app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora-DAW2';
+  title = 'Ejercicio con Texto';
+  text1="1";
+  text2="2";
+  num1=0;
+  num2=0;
+  result="X";
+
+  concatenate():void{
+    this.result=this.text1+this.text2;
+  }
+  upper():void{
+    this.result=this.text1.toUpperCase();
+  }
+  lower():void{
+    this.result=this.text1.toLowerCase();
+  }
+  find():void{
+    this.result=(this.text1.indexOf(this.text2)).toString();
+  }
+  slice():void{
+    this.result=this.text1.slice(this.num1,this.num2);
+  }
+  length():void{
+    this.result=(this.text1.length).toString();
+  }
+  include():void{
+    this.result=String(this.text1.includes(this.text2));
+  }
+  split():void{
+    this.result=((this.text1).split('this.text2')).toString();
+  }
 }
